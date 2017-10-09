427c3380b13a65011136b9f52ade5b358436ad18
TuckerBMorgan <tucker.bull.morgan@gmail.com>
2017-10-08 23:10:20 -0400

    a test for delta time

--- src/frame_timer.rs
+++ src/frame_timer.rs
@@ -1,6 +1,20 @@
 use std::time::{SystemTime, Duration};
 use std::thread::sleep;
 
+
+//John Renner
+//
+trait AsMillis {
+    fn as_millis(&self) -> u64;
+}
+
+impl AsMillis for Duration {
+    fn as_millis(&self) -> u64 {
+        self.as_secs() * 1000 + (self.subsec_nanos() / 1_000_000u32) as u64
+    }
+}
+
+
 pub struct FrameTimer {
     n_frame_per_second_length: Duration,
     //I allocate the frame_duration varible here so that I can avoid an allocation each frame
@@ -8,6 +22,7 @@ pub struct FrameTimer {
     sleep_duration: Duration,
     frame_start: SystemTime,
     frame_end: SystemTime,
+    delta_time: u64
 }
 
 impl FrameTimer {
@@ -26,7 +41,8 @@ impl FrameTimer {
             frame_duration: frame_duration,
             sleep_duration: sleep_duration,
             frame_start: frame_start,
-            frame_end: frame_end
+            frame_end: frame_end,
+            delta_time: 0u64
         }
     }
 
@@ -34,9 +50,15 @@ impl FrameTimer {
         self.frame_start = SystemTime::now();
     }
 
+    pub fn delta_time(&self) -> f32 {
+        (self.delta_time as f32 / 1000f32)
+    }
+
     pub fn frame_end(&mut self) {
+        
         self.frame_end = SystemTime::now();
         self.frame_duration = self.frame_start.duration_since(self.frame_start).unwrap();
+        self.delta_time = self.frame_duration.as_millis();
         if self.frame_duration.as_secs() > 0 {
             return;
         }
