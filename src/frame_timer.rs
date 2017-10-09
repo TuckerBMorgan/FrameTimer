47b436c477943ac9738f10600ee2f7dadeca5046
TuckerBMorgan <tucker.bull.morgan@gmail.com>
2017-10-08 22:06:49 -0400

    Some fixes to code

--- src/frame_timer.rs
+++ src/frame_timer.rs
@@ -2,39 +2,36 @@ use std::time::{SystemTime, Duration};
 use std::thread::sleep;
 
 pub struct FrameTimer {
-    sixty_frames_per_second_length: Duration,
+    n_frame_per_second_length: Duration,
+    //I allocate the frame_duration varible here so that I can avoid an allocation each frame
     frame_duration: Duration,
     sleep_duration: Duration,
     frame_start: SystemTime,
     frame_end: SystemTime,
-    delta_time: f32,
 }
 
 impl FrameTimer {
     pub fn new() -> Self {
 
         let sleep_duration = Duration::from_millis(0);
+        //TODO: Make this more then just a 60 FPS timer
         let sixty_frames_per_second_length = Duration::from_millis(16);
+        
         let frame_duration = Duration::from_millis(0);
         let frame_start = SystemTime::now();
         let frame_end = SystemTime::now();
 
         FrameTimer {
-            sixty_frames_per_second_length: sixty_frames_per_second_length,
+            n_frame_per_second_length: sixty_frames_per_second_length,
             frame_duration: frame_duration,
             sleep_duration: sleep_duration,
             frame_start: frame_start,
-            frame_end: frame_end,
-            delta_time: 0f32
+            frame_end: frame_end
         }
     }
 
     pub fn frame_start(&mut self) {
         self.frame_start = SystemTime::now();
-    }
-
-    pub fn get_delta_timer(&self) {
-        self.delta_time
     }
 
     pub fn frame_end(&mut self) {
@@ -44,8 +41,8 @@ impl FrameTimer {
             return;
         }
 
-        if self.sleep_duration.subsec_nanos() < self.sixty_frames_per_second_length.subsec_nanos() {
-            self.sleep_duration = self.sixty_frames_per_second_length - self.frame_duration;
+        if self.sleep_duration.subsec_nanos() < self.n_frame_per_second_length.subsec_nanos() {
+            self.sleep_duration = self.n_frame_per_second_length - self.frame_duration;
             sleep(self.sleep_duration);
         }
     }
